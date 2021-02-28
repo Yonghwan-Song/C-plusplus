5f459f54c42133252225dee42da974237c8d6dd7
valamorguliz <72689705+valamorguliz@users.noreply.github.com>
2021-02-28 11:24:43 -0500

    Update Car.h

--- Car.h
+++ Car.h
@@ -1,12 +1,3 @@
-/**********************************************************
- * Name: Yonghwan Song
- * Student ID: 135458198
- * Seneca email: ysong115@myseneca.ca
- * Section: NDD
- * Completion Date: 8.8
- ->I have done all the coding by myself and only copied the code that my
- professor provided to complete my workshops and assignments.
- **********************************************************/
 #ifndef SDDS_CAR_H
 #define SDDS_CAR_H
 #include "Vehicle.h"
